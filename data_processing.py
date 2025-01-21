347bb95cca6fe7de6b712c5c75910e23723025a3
Mokshaa-Joshi <122172274+Mokshaa-Joshi@users.noreply.github.com>
2025-01-21 11:09:06 +0530

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -1,8 +1,13 @@
 import pdfplumber
-pdf_path='My_Resume.pdf'
+
 def extract_text_from_pdf(pdf_path):
     text = ""
     with pdfplumber.open(pdf_path) as pdf:
         for page in pdf.pages:
             text += page.extract_text()
     return text
+
+# Use relative path for GitHub
+pdf_path = "My_Resume.pdf"
+text = extract_text_from_pdf(pdf_path)
+print(text)
